25dd485dd43fd7ee22d345457e8cb13bad8f8021
dice-gamer <temuujinsetsoyu@gmail.com>
2025-02-18 13:45:33 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -11,21 +11,27 @@
 
 
 //  Шоог аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр өгнө.
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
     // <div class="player-score" id="score-0">43</div>
 
     // Программ эхлэхэд бэлтгэе
-    document.querySelector('#score-0').textContent = 0;
-    document.querySelector('#score-1').innerHTML = 0;
-
-
-    // Цуглуулсан оноог 0 болгоё
-    document.querySelector('#current-0').innerHTML = 0; 
-    document.querySelector('#current-1').innerHTML = 0; 
+    document.getElementById('score-0').textContent = "0";
+    document.getElementById('score-1').textContent = "0";
+    document.getElementById('current-0').textContent = "0"; 
+    document.getElementById('current-1').textContent = "0"; 
 
     // Шооны зургийг харуулахаа больё
-    document.querySelector(" .dice ").style.display = 'none';
+    var diceDom = document.querySelector(" .dice");
+    diceDom.style.display = "none";
 
-    console.log('Шоо :' + dice);
+    document.querySelector(" .btn-roll ").addEventListener('click', function() {
+        var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+        diceDom.style.display = "block";
+        diceDom.src = 'dice-' + diceNumber + '.png';
+
+    });
+
+
 
